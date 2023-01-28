ee3c362a554648870c3cf2d8f11d0dbf493041de
kenndu <59485248+kenndu@users.noreply.github.com>
2023-01-28 15:28:22 -0800

    two-sum

---
+++ 1.two-sum.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        //value, index
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i)
+                return new int[] {i, map.get(target - nums[i])}; 
+        }
+
+        return null;
+    }
+}
+// @lc code=end
+
